76660ce3c88dac7d8d00a62990ecc9f2731b0ae4
Erick Caraballo <erick.caraballo@upr.edu>
2013-10-25 11:26:42 -0400

    Initial commit.

---
+++ app/models/User.java
@@ -0,0 +1,27 @@
+package models;
+
+import javax.persistence.*;
+import com.avaje.*;
+import play.db.ebean.Model;
+
+@Entity
+public class User extends Model {
+	
+	@Id
+	public String email; // notice it's public! this will be our id
+	public String name;
+	public String password;
+	
+	public User(String email, String name, String password) {
+		this.email = email;
+		this.name = name;
+		this.password = password;
+	}
+	
+	// Find will be used to programatically make querie
+	public static Finder<String, User> find = new Finder<String, User>(
+			String.class, User.class
+			);
+			
+	
+}
